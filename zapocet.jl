59add4646141ffa44c7e34cf937ae71716d456b2
strelda <strelecekja@gmail.com>
2021-01-24 23:03:39 +0100

    free particle added and suddenly the sun was shining :)

--- zapocet.jl
+++ zapocet.jl
@@ -25,7 +25,7 @@ function thomasUni(a::ComplexF64, b::Vector{ComplexF64}, d::Vector{ComplexF64}, 
   return x
 end
 
-function braket(P::Vector{ComplexF64},P0::Vector{ComplexF64},h::Float64) #only for h even
+function braket(P::Vector{ComplexF64},P0::Vector{ComplexF64},h::Float64) #Simpson rule for h even, O(h^4)
   n = length(P)
   I = (conj(P[1])*P0[1] + 4*conj(P[n-1])*P0[n-1] + conj(P[n])*P0[n])
   for i in 1:(round(Int,n/2)-1)
@@ -56,44 +56,71 @@ const p0=0
 const x0=-5
 
 #quality, reasonable are: {10000,60,1e-2,50}, {10000,1000,1e-2,500}
-const n=100000
-const scale=500 #scale up to fit the wave on the screen
-const dt=1e-2
-const tExact=2000
-const h=-2*x0/n #centralizes the potential
-const iter = round(Int,tExact/dt)
+n=500
+scale=30 #scale up to fit the wave on the screen
+dt=1e-1
+tExact=10
+h=-2*x0/n #centralizes the potential for gauss
+iter = round(Int,tExact/dt)
 
-#exact solution
+
+
+##exact solution 
+
+#eigen
+const k  = 0.1
+const En = 5
+exactψeigen(x::Float64,t::Float64) = exp(-im*En*t+im*k*x)
+ 
+#Gauss
 const X(t::Float64)  = p0*t/μ
 const Σ2(t::Float64) = σ*σ + t*t/(4μ*μ*σ*σ)
 const φ(x::Float64,t::Float64)= p0*(x-X(t)) + p0*p0*t/(2μ) + t*(x-X(t))^2/(8*μ*σ*σ*Σ2(t)) + angle(1/sqrt(μ+im*t/(2σ*σ)))
-const exactψ(x::Float64,t::Float64)= (2π*Σ2(t))^(-1/4) * exp( -(x-X(t))^2/(4*Σ2(t)) +im* φ(x,t))
-exactψgrid = Array{ComplexF64}(undef,n)
+const exactψgauss(x::Float64,t::Float64)= (2π*Σ2(t))^(-1/4) * exp( -(x-X(t))^2/(4*Σ2(t)) +im* φ(x,t))
 
 
-#functions for approximate solution
+
+##functions for approximate solution
 const V(x::Float64) = 0.5*μ*ω*ω*x*x
 v  = Vector{Float64}(undef,n)
-ψ0 = Vector{ComplexF64}(undef,n)
+ψ0eigen = Vector{ComplexF64}(undef,n)
+ψ0gauss = Vector{ComplexF64}(undef,n)
 
-for i in 1:n
-  x=scale*(x0+i*h)
-  v[i]=V(x)
-  ψ0[i] = (2π*σ*σ)^(-1/4)*exp(-x*x/(4σ*σ) + im*p0*x)
-end
+
+##initial conditions
 
 #b is diagonal of tridiagonal matrix H, a are all offdiagonal elements
-const b = (1/(μ*h^2) .+ v)/scale^2
-const a = -1/(2μ*h^2*scale^2)
-
-
-#initial conditions
-ψ = ψ0
 z = Vector{ComplexF64}(undef,n)
+exactψgrid = Array{ComplexF64}(undef,n)
 
+#eigen
+const a = 0
+b = Array{ComplexF64}(undef,n)
+for i in 1:n
+  x=scale*(x0+i*h)
+  ψ0eigen[i] = exp(im*k*x)
+  b[i] = En
+end
+ψ = copy(ψ0eigen)
+const ψ0= copy(ψ0eigen)
+exactψ = exactψeigen
+#gauss
+# a = -1/(2μ*h^2*scale^2)
+# b = (1/(μ*h^2) .+ v)/scale^2
+# for i in 1:n
+#   x=scale*(x0+i*h)
+#   v[i]=V(x)
+#   ψ0gauss[i] = (2π*σ*σ)^(-1/4)*exp(-x*x/(4σ*σ) + im*p0*x)
+# end
+# ψ = copy(ψ0gauss)
+# const ψ0= copy(ψ0gauss)
+# exactψ = exactψgauss
+  
+
+
+##iteration
 const K = 0.5*im*dt
 autocor = Array{ComplexF64}(undef,iter)
-
 cnt=0
 for tim in ProgressBar(1:iter)
   global ψ, z
